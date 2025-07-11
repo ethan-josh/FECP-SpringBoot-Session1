ec37f316c1354d114b4383df83dcdf951b43a9ed
ethan-josh <ethanjoshua.camingao@gcash.com>
2025-07-11 10:21:22 +0800

    init commit

---
+++ src/main/java/org/example/Zoo.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.*;
+
+public class Zoo {
+    public static void logInMenu() {
+        System.out.println("=== Welcome to Zoo Admin Console ===");
+        System.out.println("Please log in.");
+    }
+
+    public static void printUserMenu() {
+        System.out.println("========== 🦁 ZOO ADMIN MAIN MENU ==========");
+        System.out.println("1. Setup Zoo Staff");
+        System.out.println("2. Access Handler Module");
+        System.out.println("3. Open Zoo to Visitors");
+        System.out.println("4. Close Zoo to Visitors");
+        System.out.println("5. Exit");
+        System.out.print("Choose an option: ");
+    }
+    public static void main(String[] args) {
+        // Insert codes here
+
+    }
+}
